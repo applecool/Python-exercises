a855bbd9386c24f6caf18979bb93b8915a1744e9
applecool <kmitharsha@gmail.com>
2014-11-13 12:23:58 -0600

    Added comments to fibo_dict.py

--- fibo_dict.py
+++ fibo_dict.py
@@ -1,11 +1,13 @@
-known = {0:0,1:1}
+#Implementation of fibonacci using memos
+
+known = {0:0,1:1} #dictionary that keeps track of fibonacci numbers we already know
 
 def fibonacci(n):
 	if n in known:
-		return known[n]
+		return known[n] #if the result is already there, it returns immediately
 
-	result = fibonacci(n-1) + fibonacci(n-2)
-	known[n] = result
+	result = fibonacci(n-1) + fibonacci(n-2) #computes the new value
+	known[n] = result 
 	return result
 
 input = raw_input('Enter a number:')
