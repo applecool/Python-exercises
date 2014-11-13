e22fd5b610503bd417d0f94466b20bce3656e2d1
applecool <kmitharsha@gmail.com>
2014-11-13 00:17:52 -0600

    added comments to key_value.py

--- key_value.py
+++ key_value.py
@@ -1,3 +1,11 @@
+# Write a function that reads the words in words.txt and stores
+# them as keys in a dictionary. It doesn't matter what the values
+# are.
+# 
+# I used the python's in-built function random to generate random values
+# One can use uuid to generate random strings which can also be used to 
+# pair the keys i.e., in this case the words in the .txt file
+
 import random
 
 output = dict() #creates an empty dictionary
