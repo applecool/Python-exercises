9a182ef76de46aff8f57a455e1a04940011da5e7
applecool <kmitharsha@gmail.com>
2014-11-13 12:24:55 -0600

    Added comments to fibo_dict.py

--- fibo_dict.py
+++ fibo_dict.py
@@ -7,8 +7,8 @@ def fibonacci(n):
 		return known[n] #if the result is already there, it returns immediately
 
 	result = fibonacci(n-1) + fibonacci(n-2) #computes the new value
-	known[n] = result 
+	known[n] = result #adds the above result to the dictionary
 	return result
 
-input = raw_input('Enter a number:')
+input = raw_input('Enter a number:') #prompts for the user input
 print fibonacci(int(input))
